f868745b75b5b2653792719d279912f2ab066c28
Bubuka Sharif <sharifbubuka256@gmail.com>
2020-05-31 22:43:14 +0200

    ajax_type_ahead_1.1

---
+++ 17_ajax_type_ahead/type_ahead.js
@@ -0,0 +1,44 @@
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities = [];
+
+fetch(endpoint).then(blob => blob.json())
+               .then(data => cities.push(...data)); //spread the individual cities into the array
+console.log(cities);
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+    })
+};
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+};
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+/*
+    Probable use-case: Geo location application feature
+    Future challenge: Work with Uganda/african cities json data
+    Incorporate geolocation and return cities based on how near they are to the user
+*/
