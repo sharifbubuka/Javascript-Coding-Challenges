9fd684643825c97d77cbbafb9fdf4ff815041d46
Bubuka Sharif <sharifbubuka256@gmail.com>
2020-06-07 23:31:15 +0300

    faceapi_extra_challenge_5.1 + node.js_crash_course_extra_challenge_4.2
    Took a break off to familiarise myself with the interesting faceapi.js library using Web Dev Simplified's Youtube walk through found at https://www.youtube.com/watch?v=CVClHLwv-4I&list=PL5o285ahn_HUv1DjTW88v4DNf1KZby9Bj&index=2&t=0s.
    
    I also finished the node.js crash course. I am loving this.

--- 21_infinite_scrolling_effect/node.js_crash_course/index.js
+++ 21_infinite_scrolling_effect/node.js_crash_course/index.js
@@ -1,15 +1,74 @@
-//console.log('Hello from Node.js...');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-//Module Wrapper Function
-// (function (exports, require, module, __filename, __dirname) {
+const server = http.createServer((req, res) => {
+    // if(req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //         if(err) throw err;
+    //         console.log('Start up error:  ' + err);
+    //         res.writeHead(200, {'Content-Type': 'text/html'});
+    //         res.end(content);
+    //     })
+    // }
+    // if(req.url === '/api/users') {
+    //     const users = [
+    //         { name: 'Will Smith', age: 40},
+    //         { name: 'Elon Musky', age: 20}
+    //     ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(users));
+    // }
 
-//})
+    //Build filepath
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html': req.url);
+    let extname = path.extname(filePath);
+    let contentType = 'text/html';
 
-console.log(__filename, __dirname);
-const Person = require('./person');
+    switch (extname) {
+        case ".js":
+          contentType = "text/javascript";
+          break;
+        case ".css":
+          contentType = "text/css";
+          break;
+        case ".json":
+          contentType = "application/json";
+          break;
+        case ".png":
+          contentType = "image/png";
+          break;
+        case ".jpg":
+          contentType = "image/jpg";
+          break;
+      }
 
-const person1 = new Person('John Doe', 30);
+  // Read File
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        // Page not found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        //  Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 
-person1.greeting();
+});
 
-//21:57 
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
